2f405787cd29243871715b9764c7fe07bba86081
azimsodikov <azimsadikov@gmail.com>
2020-09-08 07:01:54 -0600

    Updates

---
+++ precache-manifest.7469c0689899fc74af7d5be43bbe3953.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056618632741c1920496e036e391fc05",
+    "url": "/restaurants-react/index.html"
+  },
+  {
+    "revision": "d1251bd52f59d3c0f3e7",
+    "url": "/restaurants-react/static/css/main.c27ad59f.chunk.css"
+  },
+  {
+    "revision": "79a669772190528c7fa1",
+    "url": "/restaurants-react/static/js/2.92d03325.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/restaurants-react/static/js/2.92d03325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1251bd52f59d3c0f3e7",
+    "url": "/restaurants-react/static/js/main.ff8a0141.chunk.js"
+  },
+  {
+    "revision": "9ce4e0a78687ae9c05a8",
+    "url": "/restaurants-react/static/js/runtime-main.5effbd99.js"
+  }
+]);
